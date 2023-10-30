0223f5539058fa07226ef7d72ef2e6f8029144ce
Rajat Rasal <rrr2417@ic.ac.uk>
2023-10-30 23:45:05 +0000

    Include bash scripts for training

---
+++ scripts/train.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+OPENAI_LOGDIR=/vol/biomedic3/rrr2417/.tmp poetry run image_train -- \
+    --data_dir /vol/biodata/data/chest_xray/mimic-cxr-jpg/files/ \
+    --log_interval 10 \
+    --save_interval 100 \
+    --batch_size 64

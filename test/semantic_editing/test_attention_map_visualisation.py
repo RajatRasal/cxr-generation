0e59e471f085f50c203fd6194d99f8fa2f4149bb
Rajat Rasal <yugiohrajat1@gmail.com>
2024-03-10 17:37:12 +0000

    rename tests for attn map visualisation

--- test/semantic_editing/test_attention_map_visualisation.py
+++ test/semantic_editing/test_attention_map_visualisation.py
@@ -89,13 +89,6 @@ def test_visualise_cross_attention_maps_nouns_clustering(
         element_name="attn",
     )
     masks = localise_nouns(clusters, cross_avg.cpu().numpy(), noun_indices)
-
-    # def resize(mask):
-    #     mask_ = torch.from_numpy(mask).float().unsqueeze(0).unsqueeze(0)
-    #     mask_ = F.interpolate(mask_, size=512, mode="nearest")
-    #     mask_ = mask_.round().bool().int().squeeze(0).squeeze(0).numpy()
-    #     return mask_
-
     masks = {k: Image.fromarray((v * 255).astype(np.uint8)) for k, v in masks.items()}
 
     fig, axes = plt.subplots(nrows=1, ncols=len(masks), figsize=(15, 5))
@@ -104,6 +97,38 @@ def test_visualise_cross_attention_maps_nouns_clustering(
     for i, (k, v) in enumerate(masks.items()):
         plot_image_on_axis(axes[i + 1], v, k.capitalize())
     fig.savefig("masks.pdf")
+
+    assert False
+
+
+def test_visualise_prompt_localisation(
+    sd_adapter_with_attn_excite,
+    cfg_ddim,
+    image_prompt_cat_and_dog,
+    attention_store,
+):
+    image, prompt = image_prompt_cat_and_dog
+    cfg_ddim.fit(image, prompt)
+    cfg_ddim.generate(prompt)
+
+    noun_indices = find_noun_indices(cfg_ddim.model, prompt)
+    print(noun_indices)
+
+    resolutions = [16, 32, 64]
+    fig, axes = plt.subplots(nrows=len(resolutions), ncols=len(noun_indices))
+    for i, res in enumerate(resolutions):
+        attn_avg = attention_store.aggregate_attention(
+            places_in_unet=["up", "down", "mid"],
+            is_cross=True,
+            res=res,
+            element_name="attn",
+        )
+        for j, (index, noun) in enumerate(noun_indices):
+            attn_map = attn_avg[:, :, index].cpu().numpy()
+            title = noun if i == 0 else ""
+            plot_image_on_axis(axes[i, j], attn_map, title)
+
+    fig.savefig("ca_maps.pdf")
 
     assert False
 

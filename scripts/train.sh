2595749d729ee867a2148703360cc81964723a3c
Rajat Rasal <yugiohrajat1@gmail.com>
2024-03-27 14:32:52 +0000

    rename notebooks to experiments

--- scripts/train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-RDMAV_FORK_SAFE=1 OPENAI_LOGDIR=/vol/biomedic3/rrr2417/.tmp poetry run image_train -- \
-    --data_dir /vol/biodata/data/chest_xray/mimic-cxr-jpg/files/ \
-    --log_interval 10 \
-    --save_interval 100 \
-    --batch_size 1
